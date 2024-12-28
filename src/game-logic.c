e17de71479666d105578c20eccd9059bdd739e70
Tim Kempkens <tim.kempkens@googlemail.com>
2024-12-28 15:48:56 +0100

    added removing full rows

--- src/game-logic.c
+++ src/game-logic.c
@@ -15,6 +15,10 @@ typedef enum
 static ActivePieceStatus get_active_piece_status(GameData *gameData);
 static void create_new_actice_piece(GameData *gameData, GameConfig *config);
 static void add_active_piece_to_fixed(GameData *gameData);
+static bool is_row_full(GridValue row[GAME_COLUMNS]);
+static int get_full_fixed_row_number(GameData *gameData);
+static void remove_line(GameData *gameData, int lineNumber);
+static int calculate_score(GameData *gameData, GameConfig *config, int lineNumber);
 
 int frame_counter = 0;
 
@@ -30,17 +34,64 @@ void nextMove(GameData *gameData, GameConfig *config)
     if (activePieceStatus == Fixed)
     {
         add_active_piece_to_fixed(gameData);
+        int fullLineNumber = get_full_fixed_row_number(gameData);
+        while (fullLineNumber != -1)
+        {
+            gameData->score += calculate_score(gameData, config, fullLineNumber);
+            remove_line(gameData, fullLineNumber);
+            fullLineNumber = get_full_fixed_row_number(gameData);
+        }
+
         create_new_actice_piece(gameData, config);
         statusOfNewPiece = get_active_piece_status(gameData);
     }
     if (activePieceStatus == GameOver || statusOfNewPiece == GameOver)
     {
         gameData->gameOver = true;
-        print_game_data(gameData);
-        g_print("GameOver");
     }
 
     frame_counter++;
+}
+
+static int calculate_score(GameData *gameData, GameConfig *config, int lineNumber)
+{
+    return 10;
+}
+
+static void remove_line(GameData *gameData, int lineNumber)
+{
+    for (int row = lineNumber; row >= 0; row--)
+    {
+        for (size_t col = 0; col < GAME_COLUMNS; col++)
+        {
+            gameData->fixedBlocks[row][col] = row == 0 ? Empty : gameData->fixedBlocks[row - 1][col];
+        }
+    }
+}
+
+static int get_full_fixed_row_number(GameData *gameData)
+{
+    for (int i = 0; i < GAME_ROWS; i++)
+    {
+        if (is_row_full(gameData->fixedBlocks[i]))
+        {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+static bool is_row_full(GridValue row[GAME_COLUMNS])
+{
+    for (size_t i = 0; i < GAME_COLUMNS; i++)
+    {
+        if (row[i] == Empty)
+        {
+            return false;
+        }
+    }
+    return true;
 }
 
 static void add_active_piece_to_fixed(GameData *gameData)
